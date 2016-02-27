f10be5e7234670d6a9a2d562dd63325996a086a4
Gustavo <theworldismine249@yahoo.com>
2016-02-27 20:58:10 +0200

    Initial commit

---
+++ src/sample/Controller.java
@@ -0,0 +1,160 @@
+package sample;
+
+import javafx.animation.Timeline;
+import javafx.geometry.Pos;
+import javafx.scene.Scene;
+import javafx.scene.control.*;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.GridPane;
+import javafx.scene.layout.VBox;
+import javafx.scene.text.Font;
+import javafx.stage.Stage;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Controller {
+    public static Stage window = new Stage();
+    public static void start() {
+        PasswordField passwordField = new PasswordField();
+        Label label = new Label("Insert the word: ");
+        Label welcome = new Label("Le wild spanzuratoare\n appears :))");
+        welcome.setFont(new Font(18));
+        label.setAlignment(Pos.TOP_CENTER);
+
+        Button enter = new Button("Enter");
+        enter.setOnAction(e -> {
+            String word = passwordField.getText();
+            if (word.isEmpty() || word == null)
+                System.out.println("Enter a word please !");
+            else
+                processWord(passwordField.getText());
+        });
+
+        GridPane gridPane = new GridPane();
+        gridPane.setVgap(10);
+        gridPane.setHgap(10);
+        gridPane.setAlignment(Pos.CENTER);
+        gridPane.setConstraints(welcome, 0, 0);
+        gridPane.setConstraints(label, 0, 1);
+        gridPane.setConstraints(passwordField, 1, 1);
+        gridPane.setConstraints(enter, 1, 2);
+        gridPane.getChildren().addAll(welcome, label, passwordField, enter);
+
+        BorderPane borderPane = new BorderPane();
+        borderPane.setCenter(gridPane);
+
+        Scene scene = new Scene(borderPane, 350, 350);
+        window.setScene(scene);
+        window.show();
+    }
+
+    private static void processWord(String word) {
+        Label wordHint = createWordHint(word);
+        List<Character> letters = new ArrayList<>();
+
+        Label label = new Label("Add a letter: ");
+        TextField letterInput = createLetterInput();
+
+        final int[] livesValue = {word.length()};
+        Label lives = new Label("Lives: " + livesValue[0]);
+        lives.setFont(new Font(20));
+
+        Button addLetter = new Button("Try");
+        addLetter.setOnAction(e -> {
+            Character letter = letterInput.getText().charAt(0);
+
+            if (word.contains("" + letter)) {
+                letters.add(letter);
+                wordHint.setText(updateWord(word, letters));
+                String text = wordHint.getText();
+                if (word.equals(text)) {
+                    newGame("You win :)", word);
+                }
+            }
+            else {
+                livesValue[0] -= 1;
+                if (livesValue[0] > 0) {
+                    lives.setText("Lives:" + livesValue[0]);
+                } else
+                    newGame("You lost :(", word);
+            }
+        });
+
+        GridPane gridPane = new GridPane();
+        gridPane.setVgap(10);
+        gridPane.setHgap(10);
+        gridPane.setAlignment(Pos.CENTER);
+        gridPane.setConstraints(wordHint, 0, 0);
+        gridPane.setConstraints(label, 0, 1);
+        gridPane.setConstraints(letterInput, 1, 1);
+        gridPane.setConstraints(lives, 1, 5);
+        gridPane.setConstraints(addLetter, 2, 1);
+        gridPane.getChildren().addAll(wordHint, label, letterInput, lives, addLetter);
+
+        BorderPane borderPane = new BorderPane();
+        borderPane.setCenter(gridPane);
+        Scene scene = new Scene(borderPane, 350, 350);
+        window.setScene(scene);
+    }
+
+
+    private static void newGame(String message, String word) {
+        VBox vBox = new VBox();
+        vBox.setAlignment(Pos.CENTER);
+        vBox.setSpacing(10);
+
+        Label label = new Label(message);
+        label.setFont(new Font(30));
+        Label winner = new Label("The word was: " + word);
+        winner.setFont(new Font(17));
+        Button newGame = new Button("Start a new game");
+        newGame.setOnAction(e -> start());
+        vBox.getChildren().addAll(label, winner, newGame);
+        Scene scene = new Scene(vBox, 350, 350);
+        window.setScene(scene);
+    }
+
+    private static String updateWord(String word, List<Character> letters) {
+        StringBuilder gaps = new StringBuilder();
+        int ok;
+
+        for (int i = 1; i < word.length() - 1; i++) {
+            ok = 0;
+            for (int j = 0; j < letters.size(); j++) {
+                if (word.charAt(i) == letters.get(j) && ok == 0) {
+                    gaps.append(letters.get(j));
+                    ok = 1;
+                }
+            }
+            if (ok == 0) {
+                gaps.append(" _ ");
+            }
+        }
+        String hint = new String("" + word.charAt(0) + gaps + word.charAt(word.length() - 1));
+        return hint;
+    }
+
+    private static TextField createLetterInput() {
+        TextField letterInput = new TextField();
+        letterInput.setPrefWidth(30);
+        letterInput.setTextFormatter(new TextFormatter<String>((TextFormatter.Change change) -> {
+            String newText = change.getControlNewText();
+            if (newText.length() > 1) {
+                return null;
+            } else {
+                return change;
+            }
+        }));
+        return letterInput;
+    }
+
+    private static Label createWordHint(String word) {
+        String blankLines = new String();
+        for (int i = 0; i < word.length() - 2; i++)
+            blankLines = blankLines.concat(" _ ");
+        Label wordHint = new Label("" + word.charAt(0) + blankLines + word.charAt(word.length() - 1));
+        wordHint.setFont(new Font(30));
+        return wordHint;
+    }
+}
